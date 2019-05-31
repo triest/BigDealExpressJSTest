f3b8410712e4c3155a3d9ad95c0e115d9daf8052
triest <triest21"gmail.com>
2019-05-31 17:09:43 +0300

    add sequlize

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,29 @@
+//import users from './db/users';
 var express = require('express');
+const Sequelize = require("sequelize");                                                     const sequelize = new Sequelize("test", "qt", "allowmetouse", {
+  dialect: "postgres",
+  host: "localhost",
+  port: "5032"
+});                                                                                                                                                                                                                                                                                    
+//var users = require('db/users');
+
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+
+
+
+
 var router = express.Router();
 
 /* GET users listing. */
@@ -12,10 +37,29 @@ router.get('/add', function(req, res, next) {
 });
 
 router.post('/add', function(req, res, next) {
+  sequelize.sync().then(result=>{
+    console.log(result);
+  })
+  .catch(err=> console.log(err));
+
   console.log(req.body); // this is what you want           
-  
+  if(!req.body.name) {                                                                                                                                                                                        
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required'
+    })
+  };
+
+  User.create({ 
+    name: "Bob",                                                                                                                                                                                                                                                          
+  }).then(res=>{
+    const user = {id: res.id, name: res.name}
+    console.log(user);
+  }).catch(err=>console.log(err));
 });
 
-
-
 module.exports = router;
+
+
+
+
