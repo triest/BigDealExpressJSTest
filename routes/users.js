1f6e4ca9ab8aee4efca1714beddf38f2c62d4f10
triest <you@example.com>
2019-06-02 15:11:03 +0300

    add delete

--- routes/users.js
+++ routes/users.js
@@ -29,131 +29,146 @@ const sequelize = new Sequelize({
 //var users = require('db/users');
 
 const UserTest = sequelize.define("userTest", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: true
-  },
-  createdAt: {
-    field: 'created_at',
-    type: Sequelize.DATE,
-},
-updatedAt: {
-    field: 'updated_at',
-    type: Sequelize.DATE,
-},
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    createdAt: {
+        field: 'created_at',
+        type: Sequelize.DATE,
+    },
+    updatedAt: {
+        field: 'updated_at',
+        type: Sequelize.DATE,
+    },
 });
-
-
-
 
 
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  sequelize.sync().then(result=>{
-  });
- UserTest.findAll({raw:true}).then(data=>{
-     res.render("index", {
-         users:data
-     });
-  }).catch(err=>console.log("error"));
+router.get('/', function (req, res, next) {
+    sequelize.sync().then(result => {
+    });
+    UserTest.findAll({raw: true}).then(data => {
+        res.render("index", {
+            users: data
+        });
+    }).catch(err => console.log("error"));
 
 });
 
 router.get('/:id', function (req, res) {
-    sequelize.sync().then(result=>{
+    sequelize.sync().then(result => {
     });
 
-    var id=req.params.id;
+    var id = req.params.id;
 
-    UserTest.findByPk(id).then(data=>{
+    UserTest.findByPk(id).then(data => {
         res.render("view", {
-            user:data
+            user: data
         });
-    }).catch(err=>console.log(err.toString()));
+    }).catch(err => console.log(err.toString()));
 });
 
 router.get('/edit/:id', function (req, res) {
-    sequelize.sync().then(result=>{
+    sequelize.sync().then(result => {
     });
 
-    var id=req.params.id;
+    var id = req.params.id;
 
-    UserTest.findByPk(id).then(data=>{
+    UserTest.findByPk(id).then(data => {
         res.render("edit", {
-            user:data
+            user: data
         });
-    }).catch(err=>console.log(err.toString()));
- /*   UserTest.update({ name: req.body.name }, {
-        where: {
-            id: id
-        }
-    }).then((res) => {
-        console.log(res);
-    });
-*/
+    }).catch(err => console.log(err.toString()));
+    /*   UserTest.update({ name: req.body.name }, {
+           where: {
+               id: id
+           }
+       }).then((res) => {
+           console.log(res);
+       });
+   */
 });
 
 
 router.put('/:id', function (req, res) {
-    sequelize.sync().then(result=>{
+    sequelize.sync().then(result => {
     });
-    let idPar=req.params.id;
-    let name=req.query.name;
+    let idPar = req.params.id;
+    let name = req.query.name;
     console.log(name);
     UserTest.update(
-        {name:name},
-        {where:{
-            id:idPar}
+        {name: name},
+        {
+            where: {
+                id: idPar
+            }
         }
-    ).
-     then()
-     .catch(console.log("err"));
-   res.send("ok");
+    ).then()
+        .catch(console.log("err"));
+    res.send("ok");
 });
 
-
-/* GET users listing. */
-router.get('/add', function(req, res, next) {
-  res.render('add_user', { title: 'Expresqs' });
+router.delete('/:id', function (req, res) {
+    let idPar = req.params.id;
+    console.log(idPar);
+    /*  UserTest.destroy(
+          {
+              where:{
+                  id:idPar
+              }
+          }
+      );
+      res.send(idPar);*/
+    UserTest.findByPk(idPar).then(data => {
+        data.destroy()
+    });
+    res.send("ok");
 });
 
-router.post('/add', function(req, res, next) {
-  sequelize.sync().then(result=>{
-    //console.log(result);
-  })
-  .catch(err=> console.log(err));
+    /* GET users listing. */
+    router.get('/add', function (req, res, next) {
+        res.render('add_user', {title: 'Expresqs'});
+    });
 
-    sequelize.sync().then(result=>{
-        //console.log(result);
-    }).catch();
+    router.post('/add', function (req, res, next) {
+        sequelize.sync().then(result => {
+            //console.log(result);
+        })
+            .catch(err => console.log(err));
+
+        sequelize.sync().then(result => {
+            //console.log(result);
+        }).catch();
 
 
-  //console.log(req.body); // this is what you want
-  if(!req.body.name) {                                                                                                                                                                                        
-    return res.status(400).send({
-      success: 'false',
-      message: 'title is required'
-    })
-  };
+        //console.log(req.body); // this is what you want
+        if (!req.body.name) {
+            return res.status(400).send({
+                success: 'false',
+                message: 'title is required'
+            })
+        }
+        ;
 
-  UserTest.create({ 
-    name: req.body.name,                                                                                                                                                                                                                                              
-  }).then(res=>{
-    const user = {id: res.id, name: res.name}
-  }).catch(err=>console.log(err));
+        UserTest.create({
+            name: req.body.name,
+        }).then(res => {
+            const user = {id: res.id, name: res.name}
+        }).catch(err => console.log(err));
 
-   res.redirect('/users')
-});
+        res.redirect('/users')
+    });
 
-module.exports = router;
+    module.exports = router;
 
 
 
