7f1294d76accf6b414a7f5a5b82303a66f8bb362
triest <triest21"gmail.com>
2019-06-05 13:42:50 +0300

    fix delete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,65 +1,22 @@
-//var User = require('../models/user');
-//var Sequelize = require("sequelize");
-
-/*
-const sequelize = new Sequelize({
-    username: 'yourname',
-    password: 'yourname',
-    database: 'test',
-    dialect: "postgres",
-    host: "localhost",
-    port: "5432"
-});
-
-User=require('../db/models/user')
-*/
-
-/*
-const User = sequelize.define("user", {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: true
-    },
-    createdAt: {
-        field: 'created_at',
-        type: Sequelize.DATE,
-    },
-    updatedAt: {
-        field: 'updated_at',
-        type: Sequelize.DATE,
-    },
-});
-*/
 
 exports.index = function (req, res) {
   const db = req.app.get('db');
   //console.log(db);
   db.models.user.findAll().then(function (data) {
-    //console.log(data)
-    if (data !== "") {
-      res.json(data)
-    }
-    else {
-      res.send("empty")
-    }
-  }).catch(err => console.log(err.toString()));
+     res.json(data)
+    }).catch(err => console.log(err.toString()));
 
 };
 
 
 exports.get = function (req, res) {
+  const db = req.app.get('db');
   var id = req.params.id;
   if (typeof id != "number") {
     res.send(400)
   }
 
-  User.findByPk(id).then(data => {
+  db.models.user.findByPk(id).then(data => {
     res.json(data);
   }).catch(err => console.log(err.toString()));
 };
@@ -91,13 +48,14 @@ exports.update = function (req, res) {
 
 exports.delete = function (req, res) {
   let idPar = req.params.id;
-
-  if (typeof idPar != "number") {
-    res.send(400)
+  const db = req.app.get('db');
+  /* if (typeof idPar != "number") {
+    res.send(400);
   }
+  */
 
-  User.findByPk(idPar).then(data => {
-    data.destroy()
+ db.models.user.findByPk(idPar).then(data => {
+    data.destroy();
   });
   res.send(200);
 };
