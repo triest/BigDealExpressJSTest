612747442cb9c3245723d0bd5f6bf22513733d0d
triest <triest21"gmail.com>
2019-06-06 11:14:04 +0300

    add async in controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize({
+const  sequelize = new Sequelize({
     username: 'yourname',
     password: 'yourname',
     database: 'test',
@@ -11,21 +11,17 @@ const sequelize = new Sequelize({
     logging: console.log
 });
 
-exports.index =  function (req, res) {
+exports.index =async function (req, res) {
   sequelize.sync().then(result=>{
-    //console.log(result);
   }).catch(err=> console.log(err));
-
   const db = req.app.get('db');
-  //console.log(db);
-  db.models.user.findAll().then(async function (data) {
-    await res.json(data)
+ await db.models.user.findAll().then( function (data) {
+     res.json(data)
     }).catch(err => console.log(err.toString()));
-
 };
 
 
-exports.get = function (req, res) {
+exports.get =async function (req, res) {
   const db = req.app.get('db');
 
   var id = req.params.id;
@@ -33,54 +29,55 @@ exports.get = function (req, res) {
     res.send(400)
   }
 
-  db.models.user.findByPk(id).then(data => {
+  await db.models.user.findByPk(id).then( data => {
     res.json(data);
   }).catch(err => console.log(err.toString()));
 };
 
-exports.editPage = function (req, res) {
+exports.editPage =async function (req, res) {
   if (typeof req.params.id != "number") {
     res.send(400)
   }
 
-  User.findByPk(req.params.id).then(data => {
+ await User.findByPk(req.params.id).then( data => {
     res.render("edit", {
       user: data
     });
   }).catch(err => console.log(err.toString()));
 };
 
-exports.update = function (req, res) {
+exports.update =async function (req, res) {
   var idPar = req.params.id;
   var Myname = req.query.name;
   if (typeof idPar != "number" && typeof Myname != "string") {
     res.send(400)
   }
   const db = req.app.get('db');
-  db.models.user.findByPk(idPar).then(data => {
+  await  db.models.user.findByPk(idPar).then( data => {
     data.update({ name: Myname })
   });
   res.send("ok");
 };
 
-exports.delete = function (req, res) {
+exports.delete =async function (req, res) {
   let idPar = req.params.id;
   const db = req.app.get('db');
 
- db.models.user.findByPk(idPar).then(data => {
-    data.destroy();
+   await db.models.user.findByPk(idPar).then( data => {
+     data.destroy();
   });
   res.send(200);
 };
 
-exports.create = function (req, res, next) {
+exports.create =async function (req, res, next) {
   const db = req.app.get('db');
   let data = req.body; // here is your data
 
   let name = data["name"];
-  db.models.user.create({
+ await db.models.user.create( {
     name: name,
   }).then(res => {
+    res.send(200)
   }).catch(err => console.log(err));
 }
 
--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ const userController = require('../controllers/userController.js');
 // определяем маршруты и их обработчики внутри роутера userRouter
 //router.use("/create", userController.addUser);
 
-router.get("/", userController.index);
+router.get("/",userController.index);
 router.get('/:id', userController.get);
 router.get('/edit/:id',userController.editPage);
 router.put('/:id', userController.update);
